1ec99589bf118949f6cc99dda44e126d9f31f537
Sommy-D <ogbatuanthony@gmail.com>
2022-12-21 04:28:26 -0800

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,8 @@
 
 /**
  * _islower -> checks for lowercase character
- * @c - a character argument
- * return: returns 1 and 2 depending on condition
+ * @c: a character argument
+ * Return: returns 1 and 0 depending on condition
  */
 int _islower(int c)
 {
