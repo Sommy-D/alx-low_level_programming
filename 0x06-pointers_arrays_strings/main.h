ebfb89100030f599c95df301d7cc30d28f95efa0
Sommy-D <ogbatuanthony@gmail.com>
2022-12-23 02:48:14 -0800

    Task 1

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef "MAIN_H_INCLUDED
-#define "MAIN_H_INCLUDED
+#ifndef "MAIN_H
+#define "MAIN_H
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
