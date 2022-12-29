3093862f8d5214b5d1e15cd6f219a720dd0b360d
Sommy-D <ogbatuanthony@gmail.com>
2022-12-28 20:46:31 -0800

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,17 +7,14 @@
  * Return: a string
  */
 char *_strcat(char *dest, const char *src)
+
 {
-	  int len = 0, i;
-
-        while (dest[len])
-                len++;
-
-        for (i = 0; src[i] != '0'; i++)
-        {
-                dest[len] = src[i];
-                len += 1;
-        }
-        dest[len] = 0;
+       	int index = 0, dest_len = 0;
+       	while (dest[index++])
+		  dest_len++;
+	
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+       
 	return (dest);
 }
