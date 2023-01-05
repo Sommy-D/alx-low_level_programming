6987379551cf88f3d5f424401debd1b5222fcb89
Sommy-D <ogbatuanthony@gmail.com>
2023-01-05 03:34:33 -1200

    Task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion -> functions that returns the length of a string
+ * _print_rev_recursion -> function that prints a string in reverse
  * @s: string
  * Return: void
  */
