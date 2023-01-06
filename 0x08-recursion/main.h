b4031e292f9b24a968e4ca47cdd24b40df49bcfd
Sommy-D <ogbatuanthony@gmail.com>
2023-01-05 16:56:56 -1200

    Task 6

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,6 +10,6 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int square(int n, int val);
 int is_prime_number(int n);
-int is_divisible(int num, int div);
+int evaluate_num(int num, int iterator);
 
 #endif
